e3932e1ef619a4a745dbd5afb18976d896f0bd3f
hyun <apfhd0257@naver.com>
2025-03-18 20:15:53 +0900

    style: introduce toggle

--- src/component/popup/Introduce.tsx
+++ src/component/popup/Introduce.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+
 const Introduce = ({subject, date, skill}: any) => {
-	return(
-  <div className="wy__introduce">
-    <p>{subject}</p>
-    <p>{date}</p>
-    <p>{skill}</p>
-  </div>
+	const [isToggled, setToggle] = useState<boolean>(true);
+  const handleToggle = () => {
+    setToggle(!isToggled);
+  }
+  return(
+    <div className="wy__introduce">
+      {
+        isToggled? 
+        <div className="wy__introduce__inner">
+          <p>{subject}</p>
+          <p>{date}</p>
+          <p>{skill}</p>
+          <button onClick={handleToggle}>toggle</button>
+        </div>
+      :
+      <>
+      <button className="active" onClick={handleToggle}>toggle</button>
+      </>
+      }
+    </div>
+  
 	)
 };
 
